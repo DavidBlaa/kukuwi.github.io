884a5c86d37b190e0d1eb7c97aae581d4cc42961
Jonas Niemeyer <jonas.niemeyer@uni-jena.de>
2024-11-29 23:14:06 +0100

    added more drum patterns

--- src/lib/data/pattern.ts
+++ src/lib/data/pattern.ts
@@ -50,7 +50,68 @@ export const pattern_list: pattern_type[] = [
 		audio_src: '2_Techno 2.wav',
 		instrument_type: 'drum',
 		n_instruments: 2
+	},
+	{
+		midi_src: '3_AmenBreaks.mid',
+		audio_src: '3_Amen Breaks.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '3_Custom1.mid',
+		audio_src: '3_Custom 1.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '3_Rock1.mid',
+		audio_src: '3_Rock 1.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '3_Rumba.mid',
+		audio_src: '3_Rumba.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '3_SiberianNights.mid',
+		audio_src: '3_Siberian Nights.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '3_SonClave.mid',
+		audio_src: '3_Son Clave.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '4_FunkyDrummer.mid',
+		audio_src: '4_Funky Drummer.wav',
+		instrument_type: 'drum',
+		n_instruments: 3
+	},
+	{
+		midi_src: '4_House1.mid',
+		audio_src: '4_House 1.wav',
+		instrument_type: 'drum',
+		n_instruments: 4
+	},
+	{
+		midi_src: '4_House2.mid',
+		audio_src: '4_House 2.wav',
+		instrument_type: 'drum',
+		n_instruments: 4
+	},
+	{
+		midi_src: '4_Rock2.mid',
+		audio_src: '4_Rock 2.wav',
+		instrument_type: 'drum',
+		n_instruments: 4
 	}
+
 
 ];
 
