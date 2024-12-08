f2956c11567fd38985267a9face1bb1bbf46416f
Jonas Niemeyer <jonas.niemeyer@uni-jena.de>
2024-12-08 14:53:31 +0100

    added Highscore Table

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+import type { Highscore_type } from '$lib/types/types';
+import Crown from '$lib/components/Crown.svelte';
+
+	let {
+		data
+	}: {
+
+		data: ArrayLike<Highscore_type>;
+	} = $props();
+
+
+
+
+</script>
+
+
+  <div class="h-fit w-full rounded-2xl border-2 border-gray-200 py-4 shadow-xl bg-kukuwi-blue overflow-auto">
+		<h1 class="flex flex-row items-center justify-center text-center font-mono text-7xl font-extrabold text-white">Bestenliste
+    <Crown></Crown>
+    </h1>
+		<table class="h-full w-full">
+      <thead>
+        <tr class="text-2xl uppercase text-white">
+
+            <th class="px-4 py-6 underline">Name</th>
+						<th class="px-4 py-6 underline">Punkte</th>
+
+        </tr>
+      </thead>
+      <tbody>
+        {#each data as row}
+          <tr class="text-1xl text-center text-white {row.highlight?'bg-green-500':''}">
+              <td class="px-2 py-3">{row.name}</td>
+						<td class="px-2 py-3">{row.points}</td>
+          </tr>
+				{/each}
+			</tbody>
+    </table>
+	</div>
+
---
+++ src/routes/Bestenliste/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+	import { highscore_table } from '$lib/stores/HighscoreStore.ts';
+	import type { Highscore_type } from '$lib/types/types';
+	import Table from '$lib/components/Table.svelte';
+	import Button3d from '$lib/components/Button3d.svelte';
+	import {FaSolidArrowLeft} from 'svelte-icons-pack/fa';
+	import { Icon } from 'svelte-icons-pack';
+
+	let table: Highscore_type[] = $highscore_table;
+	table.sort((a: Highscore_type, b: Highscore_type) => (a.points > b.points) ? -1 : 1);
+
+</script>
+
+<main class="flex flex-col justify-evenly items-center bg-[#FBF6EC] h-screen">
+
+	<div class="mt-3 flex w-1/2">
+		<Table
+			data={table}
+		/>
+	</div>
+	<div class="flex flex-col">
+	<Button3d bgBack = {"bg-kukuwi-yellow-dark"} bgFront = {"bg-kukuwi-yellow"} padding={"p-5"}>
+
+		<Icon src={FaSolidArrowLeft} color={"white"}/>
+
+	</Button3d>
+	</div>
+</main>
