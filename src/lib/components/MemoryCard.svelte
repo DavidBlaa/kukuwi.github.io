48d4ad72dab123780c53332fc6c6d4086bd725f3
Jonas Niemeyer <jonas.niemeyer@uni-jena.de>
2025-01-04 15:41:06 +0100

    finished memory game

--- src/lib/components/MemoryCard.svelte
+++ src/lib/components/MemoryCard.svelte
@@ -1,84 +1,84 @@
 <script lang="ts">
   import { base } from '$app/paths';
 
-  interface component_prop  {
+  interface component_prop {
 
-    isRotated: boolean;
-    paused:boolean;
-    onmouseup: (...args: any[]) => any;
-    audioSource:string;
-    finished:boolean
+		isRotated: boolean;
+		paused: boolean;
+		onmouseup: (...args: any[]) => any;
+		audioSource: string;
+		finished: boolean;
 
 
-  }
+	}
 
-  let {
+	let {
 
-    isRotated,
-    paused = $bindable(true),
-    onmouseup,
-    audioSource,
-    finished
+		isRotated,
+		paused = $bindable(true),
+		onmouseup,
+		audioSource,
+		finished
 
-  }:component_prop = $props()
-
+	}: component_prop = $props();
 
 
 </script>
 
-<div class="flex flex-col justify-center items-center justify-items-center">
+<div class="flex flex-col items-center justify-center justify-items-center">
 
-<button
-  class="w-[100px] h-[100px]  cursor-pointer perspective-1000 flex flex-col justify-center items-center"
-  onmouseup={onmouseup}
->
-  <!-- 3D Container -->
-  <div
-    class={`absolute w-full h-full transform-style-3d transition-transform duration-500 ${
+	<button
+		class="flex cursor-pointer flex-col items-center justify-center w-[200px] h-[200px] perspective-1000"
+		onmouseup={onmouseup}
+	>
+		<!-- 3D Container -->
+		<div
+			class={`absolute w-full h-full transform-style-3d transition-transform duration-500 ${
       isRotated ? 'rotate-y-180' : ''
     }`}
-  >
-    <!-- Front Side -->
-    <div class="absolute w-full h-full backface-hidden border-blue-900 border-8 rounded-xl">
-      <img
-        src={base+"/icons/Memory/Front.png"}
-        alt="Front"
-        class="w-full h-full object-cover rounded-lg shadow-lg"
-      />
-    </div>
+		>
+			<!-- Front Side -->
+			<div class="absolute h-full w-full rounded-xl border-8 border-blue-900 backface-hidden">
+				<img
+					alt="Front"
+					class="h-full w-full object-cover shadow-lg"
+					src={base+"/icons/Memory/200w/Front200.webp"}
+				/>
+			</div>
 
-    <!-- Back Side -->
-    <div class="absolute w-full h-full backface-hidden transform rotate-y-180 rounded-xl border-8 {finished?'border-gray-600': 'border-green-700'}">
-      <img
-        src={finished?base+"/icons/Memory/Back_deactivated.png":base+"/icons/Memory/Back.png"}
-        alt="Back"
-        class="w-full h-full object-cover rounded-lg shadow-lg"
-      />
-    </div>
-  </div>
-  <audio
+			<!-- Back Side -->
+			<div
+				class="absolute w-full h-full backface-hidden transform rotate-y-180 rounded-xl border-8 {finished?'border-gray-600': 'border-green-700'}">
+				<img
+					alt="Back"
+					class="h-full w-full object-cover shadow-lg"
+					src={finished?base+"/icons/Memory/200w/Back-Gray200.webp":base+"/icons/Memory/200w/Back200.webp"}
+				/>
+			</div>
+		</div>
+		<audio
 
-    src={base+audioSource}
-    bind:paused
-  ></audio>
-</button>
+			bind:paused
+			src={base+audioSource}
+		></audio>
+	</button>
 
 </div>
 
 <style>
-  .perspective-1000 {
-    perspective: 1000px;
-  }
+    .perspective-1000 {
+        perspective: 1000px;
+    }
 
-  .transform-style-3d {
-    transform-style: preserve-3d;
-  }
+    .transform-style-3d {
+        transform-style: preserve-3d;
+    }
 
-  .backface-hidden {
-    backface-visibility: hidden;
-  }
+    .backface-hidden {
+        backface-visibility: hidden;
+    }
 
-  .rotate-y-180 {
-    transform: rotateY(180deg);
-  }
+    .rotate-y-180 {
+        transform: rotateY(180deg);
+    }
 </style>
---
+++ src/routes/Memory/+page.svelte
@@ -0,0 +1,156 @@
+<script lang="ts">
+
+
+	import MemoryCard from '$lib/components/MemoryCard.svelte';
+	import { onMount } from 'svelte';
+	import { MemoryCardsData } from '$lib/data/MemoryCards';
+	import { shuffleArray } from '$lib/utils/helperfunctions';
+	import { getRandomIndex, getRandomSubset } from '$lib/utils/helperfunctions.js';
+	import GIF from '$lib/components/GIF.svelte';
+	import { goto } from '$app/navigation';
+
+
+	let cards: {
+		genre: string;
+		paused: boolean;
+		isRotated: boolean,
+		index: number,
+		audioPath: string,
+		finished: boolean
+	}[] = $state([]);
+
+
+	let lastID: number = $state(-1);
+	let cardTurned: number = $state(0);
+	let genre: string = $state('');
+	let n_cards: number = $state(0);
+	let done:boolean = $state(false);
+	let volume: number = $state(1);
+
+
+	function handleGIFButtonClick() {
+
+		goto("/");
+
+	}
+
+	onMount(() => {
+
+		for (const element of getRandomSubset(MemoryCardsData, 4)) {
+
+			cards.push({
+				index: -1,
+				genre: element.genre,
+				paused: true,
+				isRotated: false,
+				audioPath: element.audioPaths[getRandomIndex(element.audioPaths)],
+				finished: false
+			});
+			cards.push({
+				index: -1,
+				genre: element.genre,
+				paused: true,
+				isRotated: false,
+				audioPath: element.audioPaths[getRandomIndex(element.audioPaths)],
+				finished: false
+			});
+
+		}
+		cards = shuffleArray(cards);
+
+		for (const [index, card] of cards.entries()) {
+
+			card.index = index;
+
+		}
+		n_cards = cards.length;
+	});
+
+
+	function handleCLick(index: number): void {
+
+		if (cards[index].finished) {
+			return;
+		}
+
+		if (lastID == index) {
+
+			return;
+		}
+
+		if (cardTurned == 2) {
+			cardTurned = 0;
+			for (const card of cards) {
+				if (!card.finished) {
+					card.isRotated = false;
+				}
+
+			}
+		}
+		for (const card of cards) {
+
+			card.paused = true;
+
+		}
+		cards[index].isRotated = !cards[index].isRotated;
+		cards[index].paused = !cards[index].paused;
+
+		if (cardTurned == 0) {
+			cardTurned = 1;
+			genre = cards[index].genre;
+			lastID = index;
+		} else if (cardTurned == 1) {
+			cardTurned = 2;
+			if (genre === cards[index].genre) {
+				cards[index].finished = true;
+				cards[lastID].finished = true;
+				n_cards -= 2;
+				console.log(n_cards)
+
+			}
+
+		}
+
+		if(n_cards == 0){
+
+			for (const card of cards) {
+
+				card.paused = true;
+
+			}
+
+			done = true;
+
+		}
+
+	}
+
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center justify-items-center">
+	<div class="grid h-fit w-fit grid-cols-4 gap-5 rounded-xl bg-gray-300 p-12">
+		{#each cards as c}
+			<MemoryCard
+
+				isRotated={c.isRotated}
+				paused={c.paused}
+				onmouseup={() => handleCLick(c.index)}
+				audioSource={"/audios/"+c.audioPath}
+				finished={c.finished}
+
+			/>
+		{/each}
+	</div>
+		{#if done}
+		<div class="absolute top-0 left-0">
+			<GIF
+				bind:success={done}
+				onclick={handleGIFButtonClick}
+				GIFButtonTextSuccess = {"Gewonnen"}
+				GIFButtonTextFailure = {" "}
+				gifSoundPaused = {false}
+				volume = {volume}
+			/>
+		</div>
+	{/if}
+</div>
